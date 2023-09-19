0499c7c6f217d13afc4531193acff9cb1268833a
Mikkel Toft <mikto@itu.dk>
2023-09-19 11:40:10 +0200

    Possible fix for relavtive path, needs to test on mac

--- src/CSVDB/CSVDatabase.cs
+++ src/CSVDB/CSVDatabase.cs
@@ -1,63 +1,42 @@
-
 using System.Globalization;
 using CsvHelper;
 using CsvHelper.Configuration;
-namespace SimpleDB;
+using System.IO;
 
-
-public sealed class CSVDatabase<T> : IDatabaseRepository<T>
+namespace SimpleDB
 {
-    string filename = "/Users/victorlacour1/Desktop/Chirp/data/chirp_cli_db.csv";
-    public IEnumerable<T> Read(int? limit = null)
+    public sealed class CSVDatabase<T> : IDatabaseRepository<T>
     {
-        var config = new CsvConfiguration(CultureInfo.InvariantCulture);
-        using (var reader = new StreamReader(filename))
-        using (var csv = new CsvReader(reader, config))
-        {
-            var records = csv.GetRecords<T>().ToList();
-            return records;
-        }
-    }
+        private string file;
+        private string filename = "data\\chirp_cli_db.csv";
 
-    public void Store(T record)
-    {
-        Console.WriteLine("howdy.");
-        using (var writer = new StreamWriter(filename, true))
-        using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+        public CSVDatabase()
         {
-            csv.WriteRecord(record);
-            writer.Write("\n");
+            var projectFolder = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName;
+            file = Path.Combine(projectFolder, filename);
+            //file = "C:\\Users\\Bruger\\OneDrive\\Documents\\.ITU\\3.Semester\\Software_Architecture\\Chirp\\data\\chirp_cli_db.csv";
+        }
+
+        public IEnumerable<T> Read(int? limit = null)
+        {
+            var config = new CsvConfiguration(CultureInfo.InvariantCulture);
+            using (var reader = new StreamReader(file))
+            using (var csv = new CsvReader(reader, config))
+            {
+                var records = csv.GetRecords<T>().ToList();
+                return records;
+            }
+        }
+
+        public void Store(T record)
+        {
+            Console.WriteLine("howdy.");
+            using (var writer = new StreamWriter(file, true))
+            using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+            {
+                csv.WriteRecord(record);
+                writer.Write("\n");
+            }
         }
     }
 }
-
-
-/* 
-
-using System.Globalization;
-using CsvHelper;
-using CsvHelper.Configuration;
-
-public class CSVParser {
-    string filename = "data/chirp_cli_db.csv";
-
-    public List<Cheep> read() {
-        var config = new CsvConfiguration(CultureInfo.InvariantCulture);
-        using (var reader = new StreamReader(filename))
-        using (var csv = new CsvReader(reader, config))
-        {
-            var records = csv.GetRecords<Cheep>().ToList();
-            return records;
-        }
-    }
-
-    public void write(Cheep record) {
-        var records = read();
-        using (var writer = new StreamWriter(filename))
-        using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
-        {
-            records.Add(record);
-            csv.WriteRecords(records);
-        }
-    }
-}*/
