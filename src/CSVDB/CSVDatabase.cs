4e695443d7993e43cabcb1b02a8729e4afad7bd5
Mikkel Toft <mikto@itu.dk>
2023-09-19 13:19:15 +0200

    merged relative path branch with main

--- src/CSVDB/CSVDatabase.cs
+++ src/CSVDB/CSVDatabase.cs
@@ -1,9 +1,7 @@
-
 using System.Globalization;
 using CsvHelper;
 using CsvHelper.Configuration;
-namespace SimpleDB;
-
+using System.IO;
 
 namespace SimpleDB
 {
@@ -36,46 +34,15 @@ namespace SimpleDB
             }
         }
 
-    public void Store(T record)
-    {
-        Console.WriteLine("howdy.");
-        using (var writer = new StreamWriter(filename, true))
-        using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+        public void Store(T record)
         {
-            csv.WriteRecord(record);
-            writer.Write("\n");
+            Console.WriteLine("Your cheep has been stored.");
+            using (var writer = new StreamWriter(file, true))
+            using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+            {
+                csv.WriteRecord(record);
+                writer.Write("\n");
+            }
         }
-    }
-};
+    };
 }
-
-
-/* 
-
-using System.Globalization;
-using CsvHelper;
-using CsvHelper.Configuration;
-
-public class CSVParser {
-    string filename = "data/chirp_cli_db.csv";
-
-    public List<Cheep> read() {
-        var config = new CsvConfiguration(CultureInfo.InvariantCulture);
-        using (var reader = new StreamReader(filename))
-        using (var csv = new CsvReader(reader, config))
-        {
-            var records = csv.GetRecords<Cheep>().ToList();
-            return records;
-        }
-    }
-
-    public void write(Cheep record) {
-        var records = read();
-        using (var writer = new StreamWriter(filename))
-        using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
-        {
-            records.Add(record);
-            csv.WriteRecords(records);
-        }
-    }
-}*/
