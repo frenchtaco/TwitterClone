d4e70d31bb04681a39c94b59e726cf8c6f0b4740
Emil Ladegaard <elad@itu.dk>
2023-09-21 11:21:07 +0200

    Created an XUnit Testing folder for all our tests.

---
+++ tests/CSVDB.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace CSVDB.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ tests/Chirp.CLI.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Chirp.CLI.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        int number = 10;
+        Assert.Equal(10, number);
+    }
+}
