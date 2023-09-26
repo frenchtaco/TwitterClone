c9467a682ec272822a8659be0ac6b4b6e32763fb
Victor Kumar la Cour <vicl@itu.dk>
2023-09-26 21:35:08 +0200

    Made the GET work with HTTP! Co-authored-by: Emil <elad@itu.dk> Co-authored-by: Kåre

--- src/Chirp.CLI/CommandHandler.cs
+++ src/Chirp.CLI/CommandHandler.cs
@@ -1,60 +1,50 @@
-using SimpleDB;
 
-namespace CommandHandle;
 
-public class CommandHandler 
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace CommandHandle
 {
-    private int commandCounter = 0;
-
-         // This long line is called a reflection, and it forces the CSVDatabase's constructor to create an instance even though the constructor is PRIVATE.
-        // this is smart because by using this, we keep singleton property and can still use the method.
-        CSVDatabase<Cheep> database_cheeps =  (CSVDatabase<Cheep>)Activator.CreateInstance(typeof(CSVDatabase<Cheep>), nonPublic: true);//because something
-
-
-    public void Fondle(bool shouldReadOne, bool shouldReadAll, string msg)
+    public class CommandHandler
     {
-        // If "--readall" option is set, read all "cheeps" from the database.
-        if (shouldReadAll)
+        private readonly HttpClient client;
+
+        public CommandHandler()
         {
-            IEnumerable<Cheep> cheeps = database_cheeps.Read();
-            foreach (Cheep cheep in cheeps)
+            // Create the HttpClient with a base address.
+            client = new HttpClient();
+            client.BaseAddress = new Uri("http://localhost:3000"); // Replace with your server's URL
+        }
+
+        public async Task Fondle(bool shouldReadOne, bool shouldReadAll, string msg)
+        {
+            if (shouldReadAll)
             {
-                commandCounter++;
-                Console.WriteLine(cheep);
+                Console.WriteLine("1) Entered if-statement");
+                try
+                {
+                    Console.WriteLine("2) Entered try-block; pre");
+                    // Make the GET request to the /cheeps endpoint.
+                    HttpResponseMessage response = await client.GetAsync("/cheeps");
+
+                    // Check if the response is successful (status code 200).
+                        // Read and print the response content.
+                        string responseBody = await response.Content.ReadAsStringAsync();
+                        Console.WriteLine("Response: " + responseBody);
+                        Console.WriteLine("3) Entered try-block; post");
+                   
+                }
+                catch (HttpRequestException e)
+                {
+                    Console.WriteLine("\nException Caught!");
+                    Console.WriteLine("Message :{0} ", e.Message);
+                }
+            }
+            else
+            {
+                Console.WriteLine("ERROR IN FONDLE");
             }
         }
-        // If "--readone" option is set, read only the first "cheep" from the database.
-        else if (shouldReadOne)
-        {
-            IEnumerable<Cheep> cheeps = database_cheeps.Read();
-            Console.WriteLine(cheeps.ElementAt(0));
-        }
-        // If "--cheep" option is set with a message, store that message to the database.
-        else if (msg != null)
-        {
-            try
-            {
-                string author = Environment.UserName;
-                var timestamp = DateTime.Now;
-                long unixTime = ((DateTimeOffset)timestamp).ToUnixTimeSeconds();
-                var cheep = new Cheep(author, msg.ToString(), unixTime);
-                database_cheeps.Store(cheep);
-            }
-            catch (IOException e)
-            {
-                Console.WriteLine("The file does not exist");
-                Console.WriteLine(e.Message);
-            }
-        }
-    }
-
-    public int getCommandCounter()
-    {
-        return commandCounter;
-    }
-
-    public CSVDatabase<Cheep> GetCSVDatabaseWithCheeps()
-    {
-        return database_cheeps;
     }
 }
