56a765aba5c2e1b7873c4d3b4acbb3a70037ae17
Kåre Kejser <kaaj@itu.dk>
2023-10-10 12:52:56 +0200

    Implement pagination
    Co-authored-by: Emil <elad@itu.dk>

--- src/Chirp.WebService/Pages/Public.cshtml.cs
+++ src/Chirp.WebService/Pages/Public.cshtml.cs
@@ -2,25 +2,38 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using DBContext;
 using Chirpin.Models;
-using Microsoft.EntityFrameworkCore;
 
 namespace Chirp.Razor.Pages;
 
 public class PublicModel : PageModel
 {
     private readonly DatabaseContext _context;
-    public IList<Author> Authors { get; set; } = null!;
+    public List<Cheep> Cheeps { get; set; } = null!;
+    public int cheepsPerPage;
+    public int totalCheeps;
+    public int lastPage;
 
     public PublicModel(DatabaseContext context)
     {
         _context = context;
     }
 
-    public IActionResult OnGet()
+    public IActionResult OnGet([FromQuery] int page)
     {
         // Allows access to our Authors and their subsequent Cheeps:
-        Authors = _context.Authors.Include(author => author.Cheeps).ToList();
+        Cheeps = _context.Cheeps.ToList();
         
+        if (Cheeps.Count >= page * 32) 
+        {
+            Cheeps = Cheeps.GetRange((page - 1) * 32, 32);
+        }
+        else 
+        {
+            int cheepsLeft = 32 - (page * 32 - Cheeps.Count);
+            Cheeps = Cheeps.GetRange((page - 1) * 32, cheepsLeft);
+        }
+
+        int totalCheeps = Cheeps.Count;
 
         return Page();
     }
--- src/Chirp.WebService/Pages/UserTimeline.cshtml.cs
+++ src/Chirp.WebService/Pages/UserTimeline.cshtml.cs
@@ -2,22 +2,35 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using DBContext;
 using Chirpin.Models;
-
 namespace Chirp.Razor.Pages;
 
 public class UserTimelineModel : PageModel
 {
     private readonly DatabaseContext _context;
-    public Cheep Cheeps { get; set; }
-    public Author Authors { get; set; }
+    public List<Cheep> Cheeps { get; set; } = null!;
+    public int cheepsPerPage;
+    public int totalCheeps;
+    public int lastPage;
 
     public UserTimelineModel(DatabaseContext context)
     {
         _context = context;
     }
 
-    public IActionResult OnPost()
+    public IActionResult OnGet(String author, [FromQuery] int page)
     {
+        Cheeps = _context.Cheeps.Where(cheep => cheep.Author.Name == author).ToList();
+
+        if (Cheeps.Count >= page * 32) 
+        {
+            Cheeps = Cheeps.GetRange((page - 1) * 32, 32);
+        }
+        else 
+        {
+            int cheepsLeft = 32 - (page * 32 - Cheeps.Count);
+            Cheeps = Cheeps.GetRange((page - 1) * 32, cheepsLeft);
+        }
+
         return Page();
     }
 }
