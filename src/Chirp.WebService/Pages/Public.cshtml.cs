fddb744dec2df70e28933db711708f0083b17072
Mikkel Toft <mikto@itu.dk>
2023-10-10 13:56:57 +0200

    Updated razorpage for public timeline, now displays cheeps as intented

--- src/Chirp.WebService/Pages/Public.cshtml.cs
+++ src/Chirp.WebService/Pages/Public.cshtml.cs
@@ -10,6 +10,7 @@ public class PublicModel : PageModel
 {
     private readonly DatabaseContext _context;
     public IList<Author> Authors { get; set; } = null!;
+    public IList<Cheep> Cheeps { get; set; } = null!;
 
     public PublicModel(DatabaseContext context)
     {
@@ -20,7 +21,7 @@ public class PublicModel : PageModel
     {
         // Allows access to our Authors and their subsequent Cheeps:
         Authors = _context.Authors.Include(author => author.Cheeps).ToList();
-        
+        Cheeps = _context.Cheeps.Include(cheeps => cheeps.Author).ToList();
 
         return Page();
     }
