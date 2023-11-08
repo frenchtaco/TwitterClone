5f6084e26ac6c1d482cc881bf86bcc4a7260c13d
Emil Ladegaard <elad@itu.dk>
2023-11-08 17:45:14 +0100

    Made the POST Request not explode everything.

--- src/Chirp.Web/Pages/Public.cshtml.cs
+++ src/Chirp.Web/Pages/Public.cshtml.cs
@@ -1,32 +1,35 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
-using DBContext;
 using Chirp.Models;
 using Chirp.Interfaces;
-using Chirp.Infrastructure;
 
 namespace Chirp.Web.Pages;
 
 public class PublicModel : PageModel
 {
+    private readonly ILogger<PublicModel> _logger;
     private readonly ICheepRepository _cheepRepository;
     private readonly IAuthorRepository _authorRepository;
+    private readonly UserManager<Author> _userManager;
     public List<Cheep> Cheeps { get; set; } = null!;
     public int totalCheeps;
     public int cheepsPerPage;
 
-    public PublicModel(ICheepRepository cheepRepository, IAuthorRepository authorRepository)
+    public PublicModel(ICheepRepository cheepRepository, IAuthorRepository authorRepository, UserManager<Author> userManager, ILogger<PublicModel> logger)
     {
         _cheepRepository = cheepRepository;
         _authorRepository = authorRepository;
+        _userManager = userManager;
+        _logger = logger;
 
         cheepsPerPage = cheepRepository.CheepsPerPage();
     }
 
     public async Task<IActionResult> OnGet([FromQuery] int page)
-    {
+    {        
         IEnumerable<Cheep> cheeps = await _cheepRepository.GetCheeps(page);
         Cheeps = cheeps.ToList();
 
@@ -39,8 +42,25 @@ public class PublicModel : PageModel
 
     [BindProperty]
     public string CheepText { get; set; }
-    // public async Task<IActionResult> OnPost() 
-    // {
 
-    // }
+    [BindProperty(SupportsGet = true)]
+    public int PageNumber { get; set; } = 1;
+    public async Task<IActionResult> OnPostAsync([FromQuery] int? page) 
+    {
+        if (!ModelState.IsValid)
+        {
+            return Page();
+        }
+
+        var currUser = await _userManager.GetUserAsync(User);
+
+        if(currUser != null)
+        {
+            _logger.LogInformation($"SUCCESS: User {currUser.UserName} located");
+        } else { _logger.LogInformation("ERROR: User could not be found"); }
+
+        int pageNumber = page ?? 1;
+
+        return RedirectToPage("Public", new { page = pageNumber });
+    }
 }
